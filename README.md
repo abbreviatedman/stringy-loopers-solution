c1206bf3f8362dcfbb7b4a6a918d3e89138d63f9
Colin Jaffe <balloonasaurus@gmail.com>
2020-10-05 03:01:28 -0400

    Updates solution for new cohort.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cats Cradle - Solution
+
+One possible set of solutions to the [Cats Cradle](https://github.com/ci-wdi-900/cats-cradle) exercise.
--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-function challengeBanner(n) {
-  console.log(`### Challenge ${n} ACCEPTED.`);
+function printChars(str) {
+  let i = 0;
+  while (i < str.length) {
+    console.log(str[i]);
+    i = i + 1;
+  }
 }
+
+// printChars('Hello.')
+// printChars('Oh hi.')
+
+function printEveryThirdChar(str) {
+  let i = 2;
+  while ( i < str.length) {
+    console.log(str[i]);
+
+    i = i + 3;
+  }
+}
+
+// printEveryThirdChar('I am the alfalfa and the omelette.');
+
+function printCharsBackwards(str) {
+  let i = str.length - 1;
+  while (i >= 0) {
+    console.log(str[i]);
+    i = i - 1;
+  }
+}
+
+// printCharsBackwards('Hello.')
+// printCharsBackwards('Oh hi.')
 
 function vowelsOnly(str) {
   const vowels = 'aeiou';
@@ -19,11 +48,31 @@ function vowelsOnly(str) {
   }
 }
 
-challengeBanner(1);
+// const str = 'Regular expressions are for term 2.';
+// vowelsOnly(str);
 
-const str = 'Regular expressions are for term 2.';
-vowelsOnly(str);
+function printAfter(start, str) {
+  let i = start;
+  while ( i < str.length) {
+    console.log(str[i]);
+    i = i + 1;
+  }
+}
 
+// printAfter(1, 'slaughter')
+// printAfter(7, "I don't love you!")
+
+function indexOfU(str) {
+  let i = 0;
+  while ( i < str.length) {
+    i = i + 1;
+    if (str[i] === 'u') {
+      console.log(i);
+    }
+  }
+}
+
+// indexOfU('You picked the wrong house, bub.');
 
 function first5VowelsOnly(str) {
   const vowels = 'aeiou';
@@ -39,25 +88,25 @@ function first5VowelsOnly(str) {
   }
 }
 
-challengeBanner(2)
 const str2 = 'Regular expressions are for term 2.';
 
-first5VowelsOnly(str);
-first5VowelsOnly('hello');
+// first5VowelsOnly(str2);
+// first5VowelsOnly('hello');
 
 
-function printEveryThirdChar(str) {
-  let i = 2;
+function firstIndexOfU(str) {
+  let i = 0;
   while ( i < str.length) {
-    console.log(str[i]);
-
-    i = i + 3;
+    i = i + 1;
+    if (str[i] === 'u') {
+      console.log(i);
+      return;
+    }
   }
 }
 
-challengeBanner(3);
-printEveryThirdChar('I am the alfalfa and the omelette.');
-
+// firstIndexOfU('You picked the wrong house, bub.');
+// firstIndexOfU('This is unheard of!');
 
 function printFirstFourAfter(start, str) {
   let count = 0;
@@ -72,38 +121,8 @@ function printFirstFourAfter(start, str) {
   }
 }
 
-challengeBanner(4);
-printFirstFourAfter(4, `Oh hi, I didn't see you there. Welcome.`)
-printFirstFourAfter(36, `Oh hi, I didn't see you there. Welcome.`)
-
-
-function indexOfU(str) {
-  let i = 0;
-  while ( i < str.length) {
-    i = i + 1;
-    if (str[i] === 'u') {
-      console.log(i);
-    }
-  }
-}
-
-challengeBanner(5);
-indexOfU('You picked the wrong house, bub.');
-
-
-function firstIndexOfU(str) {
-  let i = 0;
-  while ( i < str.length) {
-    i = i + 1;
-    if (str[i] === 'u') {
-      console.log(i);
-      return;
-    }
-  }
-}
-
-challengeBanner(6);
-firstIndexOfU('You picked the wrong house, bub.');
+// printFirstFourAfter(4, `Oh hi, I didn't see you there. Welcome.`)
+// printFirstFourAfter(36, `Oh hi, I didn't see you there. Welcome.`)
 
 
 function firstIndexOfU2(str) {
@@ -122,6 +141,5 @@ function firstIndexOfU2(str) {
   }
 }
 
-challengeBanner(7);
-firstIndexOfU2('You picked the wrong house, bub.');
-firstIndexOfU2(`I'm Canadian, eh?`);
+// firstIndexOfU2('You picked the wrong house, bub.');
+// firstIndexOfU2(`I'm Canadian, eh?`);
